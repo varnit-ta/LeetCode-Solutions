d14288d357bc6ef98a7e448893cf750ec30d1c28
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-03-23 21:17:38 +0530

    Time: 27 ms (11.40%), Space: 11.6 MB (77.76%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,20 +1,37 @@
 class Solution(object):
+    def addTwo(self, a, b):
+        if a == 1 and b == 1:
+            return [1, 0]
+        elif a == 1 and b == 0 or a == 0 and b == 1:
+            return [0, 1]
+        else:
+            return [0, 0] 
+
     def addBinary(self, a, b):
         result = ""
+
+        if len(a) < len(b):
+            a = "0"*(len(b) - len(a)) + a
+        else:
+            b = "0"*(len(a) - len(b)) + b
+
+        i = len(a)-1
         carry = 0
 
-        len_diff = len(a) - len(b)
-        if len_diff > 0:
-            b = "0" * len_diff + b
-        elif len_diff < 0:
-            a = "0" * (-len_diff) + a
+        while i >= 0:
+            ans1 = self.addTwo(int(a[i]), int(b[i]))
+            ans2 = self.addTwo(ans1[1], carry)
 
-        for i in range(len(a) - 1, -1, -1):
-            digit_sum = int(a[i]) + int(b[i]) + carry
-            result = str(digit_sum % 2) + result
-            carry = digit_sum // 2
+            if ans1[0] == 1 or ans2[0] == 1:
+                carry = 1
+            else:
+                carry = 0
 
-        if carry:
-            result = "1" + result
+            result = str(ans2[1]) + result
+
+            if i == 0 and carry == 1:
+                result = "1" + result
+
+            i -= 1
 
         return result
