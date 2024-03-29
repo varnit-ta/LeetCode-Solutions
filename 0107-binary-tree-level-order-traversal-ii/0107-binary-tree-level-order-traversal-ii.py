58233865337a02710a08aac2a8be38f132983104
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-03-29 23:20:01 +0530

    Time: 10 ms (94.20%), Space: 12.5 MB (10.50%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def traverse(self, root, level, pos, result):
+        if not root:
+            return
+
+        if len(result) < level + 1:
+            result.append([])
+        
+        result[level].append(root.val)
+
+        self.traverse(root.left, level + 1, pos * 2, result)
+        self.traverse(root.right, level + 1, pos * 2 + 1, result)
+
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+
+        result = []
+
+        self.traverse(root, 0, 0, result)
+
+        return result[::-1]
