453bcd0b8255a5bc8bc3095a719e395689154d08
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-09-15 02:18:15 +0530

    Time: 115 ms (17.56%), Space: 53.4 MB (8.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#else
+#define debug(...) 42
+#endif
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define FOR(x, a, b) for (int x = a; x < b; x++)
+#define FORR(x, arr) for (auto& x : arr)
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return true;
+
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+
+        FOR(i, 0, n) {
+            if (dp[i] == INT_MAX) continue;
+
+            int maxReach = min(i + nums[i], n - 1);
+
+            FOR(j, i + 1, maxReach + 1) {
+                if (j == n - 1) return true;
+                dp[j] = min(dp[j], dp[i] + 1);
+            }
+        }
+
+        return dp[n - 1] != INT_MAX;
+    }
+};
