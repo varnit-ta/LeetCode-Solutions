546ab1289fb4d8d6088164899cda36b680d71098
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-09-15 21:24:09 +0530

    Time: 0 ms (100%), Space: 10.5 MB (41.42%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
+
+        vector<vector<long long>> dp(ROWS, vector<long long>(COLS, 0));
+
+        for (int i = COLS - 1; i >= 0; i--) {
+            if (grid[ROWS - 1][i] != 1) dp[ROWS - 1][i] = -1;
+            else break;
+        }
+
+        for (int i = ROWS - 1; i >= 0; i--) {
+            if (grid[i][COLS - 1] != 1) dp[i][COLS - 1] = -1;
+            else break;
+        }
+
+        for (int i = ROWS - 2; i >= 0; i--) {
+            for (int j = COLS - 2; j >= 0; j--) {
+                if (grid[i][j] != 1) {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
+            }
+        }
+
+        return -dp[0][0];
+    }
+};
