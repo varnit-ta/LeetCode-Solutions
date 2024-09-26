afe8b29b0784c39c578b4255d255dc0b05e4a1ae
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-09-27 02:12:43 +0530

    Time: 4 ms (73.99%), Space: 13.3 MB (42.11%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -3,11 +3,7 @@ public:
     vector<int> grayCode(int n) {
         vector<int> res;
         int size = 1 << n;
-        
-        for (int i = 0; i < size; i++) {
-            res.push_back(i ^ (i >> 1));
-        }
-        
+        for (int i = 0; i < size; i++) res.push_back(i ^ (i >> 1));
         return res;
     }
 };
