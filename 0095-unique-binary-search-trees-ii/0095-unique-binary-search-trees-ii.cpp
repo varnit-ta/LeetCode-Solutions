44b54d8280d98bf4573ebe1bd6451403a3992bb9
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2024-09-28 18:10:19 +0530

    Time: 11 ms (65.4%), Space: 17.2 MB (98.2%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
@@ -23,7 +12,6 @@ public:
             if (left > right) {
                 return {nullptr};
             }
-
             if (dp.find({left, right}) != dp.end()) {
                 return dp[{left, right}];
             }
@@ -41,7 +29,6 @@ public:
             return res;
         };
 
-        // Call the generate function for range [1, n]
         return generate(1, n);
     }
 };
