3f7d86e3ba0c8d0dc6c408551d6aecd36e633ca1
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-01-22 01:01:05 +0530

    Time: 0 ms (100%), Space: 14.2 MB (11.9%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,22 +5,20 @@ public:
             if (left <= right) {
                 int mid = (left + right) / 2;
 
-                // Check if mid is the minimum element
                 if (mid == 0 || nums[mid] < nums[mid - 1]) {
                     if (mid == nums.size() - 1 || nums[mid] < nums[mid + 1]) {
                         return nums[mid];
                     }
                 }
 
-                // Decide which half to search
                 if (nums[mid] >= nums[left] && nums[mid] > nums[right]) {
-                    return check(mid + 1, right);  // Search right
+                    return check(mid + 1, right);
                 } else {
-                    return check(left, mid - 1);  // Search left
+                    return check(left, mid - 1);
                 }
             }
 
-            return nums[0];  // Fallback in case the array is not rotated
+            return nums[0];
         };
 
         return check(0, nums.size() - 1);
