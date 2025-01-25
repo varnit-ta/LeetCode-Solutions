826bc4fce186048665d46c82bfe197bdcfe7020d
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-01-26 02:01:47 +0530

    Time: 0 ms (100%), Space: 12 MB (37.66%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& v) {
+        int n = v.size();
+        string ans = "";
+
+        sort(v.begin(), v.end());
+        string first = v[0], last = v[n - 1];
+
+        for (int i = 0; i < (min(first.size(), last.size())); i++){
+            if (first[i] != last[i]){
+                return ans;
+            }
+            ans += first[i];
+        }
+
+        return ans;
+    }
+};
