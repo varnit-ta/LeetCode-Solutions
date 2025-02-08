3e48193841bcb91d057922e3a05d3d95a9260cf9
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-02-08 13:01:28 +0530

    Time: 57 ms (14.32%), Space: 75.7 MB (36%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -3,10 +3,8 @@ public:
     ListNode* merge(ListNode* left, ListNode* right) {
         if (!left) return right;
         if (!right) return left;
-
         ListNode* dummy = new ListNode(-1);
         ListNode* ptr = dummy;
-
         while (left && right) {
             if (left->val > right->val) {
                 ptr->next = right;
@@ -17,9 +15,7 @@ public:
             }
             ptr = ptr->next;
         }
-
         ptr->next = left ? left : right;
-
         return dummy->next;
     }
 
@@ -27,24 +23,20 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         ListNode* prev = nullptr; 
-
         while (fast && fast->next) {
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-
         if (prev) prev->next = nullptr; 
         return slow;
     }
 
     ListNode* sortList(ListNode* head) {
         if (!head || !head->next) return head;
-
         ListNode* mid = findMid(head);
         ListNode* left = sortList(head); 
         ListNode* right = sortList(mid); 
-
         return merge(left, right);
     }
 };
