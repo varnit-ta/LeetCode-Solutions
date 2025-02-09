a72c53daedaf924c949fe7b0d8a87d92e56fa1a5
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-02-09 22:13:47 +0530

    Time: 11 ms (42.18%), Space: 44.9 MB (43.99%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,12 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int cookiesNums = s.size();
         if(cookiesNums == 0)  return 0;
+
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
         int maxNum = 0;
         int cookieIndex = cookiesNums - 1;
         int childIndex = g.size() - 1;
+        
         while(cookieIndex >= 0 && childIndex >=0){
             if(s[cookieIndex] >= g[childIndex]){
                 maxNum++;
