aa6679e970dfeb6c0df0f50571d32f731fc0d24c
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-02-12 19:52:54 +0530

    Time: 1 ms (37.37%), Space: 23.3 MB (19.53%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,18 +5,19 @@ public:
         vector<int> left(n, 1), right(n, 1);
 
         for (int i = 1; i < n; i++){
-            if (ratings[i - 1] < ratings[i]) left[i] = left[i - 1] + 1;
+            if (ratings[i - 1] < ratings[i]) 
+                left[i] = left[i - 1] + 1;
         }
 
         for (int i = n-2; i >= 0 ; i--){
-            if (ratings[i] > ratings[i + 1]) right[i] = right[i+1] + 1;
+            if (ratings[i] > ratings[i + 1])
+                right[i] = right[i+1] + 1;
         }
 
         int totalCandy = 0;
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
             totalCandy += max(left[i], right[i]);
-        }
 
         return totalCandy;
     }
