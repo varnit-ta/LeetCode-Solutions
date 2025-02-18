240c5c6f8e9fc16d762421cb4a04a59d2ee1e874
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-02-18 21:41:40 +0530

    Time: 0 ms (100%), Space: 23.1 MB (23.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,27 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool ans;
-    
-    int checkBalance(TreeNode* root){
-        if(!root)
-            return 0;
+    bool ans = true;
 
-        if(!ans)
-            return 0;
-
-        int leftSubTree = checkBalance(root->left);
-        int rightSubTree = checkBalance(root->right);
-
-        if(abs(leftSubTree-rightSubTree) > 1){
-            ans = false;
-        }
-
-        return 1 + max(leftSubTree, rightSubTree);
+    bool isBalanced(TreeNode* root) {
+        if (!root) true;
+        check(root);
+        return ans;    
     }
 
-    bool isBalanced(TreeNode* root){
-        ans = true;
-        int temp = checkBalance(root);
-        return ans;
+    int check(TreeNode* root){
+        if (!root) return 0;
+
+        int left = check(root->left);
+        int right = check(root->right);
+
+        if (abs(left - right) > 1) ans = false;
+        return 1 + max(left, right);
     }
 };
