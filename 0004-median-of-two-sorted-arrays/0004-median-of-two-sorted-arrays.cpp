b7d77fffbf10bb73d3cc92c9d814604fc32fb66b
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-02-18 19:21:33 +0530

    Time: 0 ms (100%), Space: 95.1 MB (63.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& num1, vector<int>& num2) {
-        vector<double> v(num1.size() + num2.size());
-        vector<double>:: iterator it, st;
-
-        merge(num1.begin(), num1.end(), num2.begin(), num2.end(), v.begin());
-
-        int len = v.size();
-        sort(v.begin(), v.end());
-
-        if (len%2 != 0){
-            return v.at((len-1)/2);
-        }else{
-            double n1 = v.at(len/2);
-            double n2 = v.at((len-2)/2);
-
-            
-            return ((n1+n2)/2);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
         }
+
+        int sz1 = nums1.size(), sz2 = nums2.size();
+        int left = 0, right = sz1;
+
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (sz1 + sz2 + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == sz1) ? INT_MAX : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == sz2) ? INT_MAX : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((sz1 + sz2) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
+            }
+        }
+        return -1;
     }
 };
