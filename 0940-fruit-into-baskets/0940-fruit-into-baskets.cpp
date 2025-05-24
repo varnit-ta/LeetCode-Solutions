078fe2bd53b0c76b1cbef651b9668e8cebd3dff3
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-05-24 15:25:29 +0530

    Time: 43 ms (49.74%), Space: 78.9 MB (15.6%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         map<int, int> mp;
-        int left = 0, maxLen = 0;
+        int left = 0, maxNum = 0;
 
         for (int right = 0; right < fruits.size(); right++){
             mp[fruits[right]]++;
 
-            if (mp.size() <= 2){
-                maxLen = max(maxLen, right - left + 1);
+            if (mp.size() < 3){
+                maxNum = max(maxNum, right - left + 1);
             }else{
                 mp[fruits[left]]--;
                 if (mp[fruits[left]] == 0) mp.erase(fruits[left]);
@@ -16,6 +16,6 @@ public:
             }
         }
 
-        return maxLen;
+        return maxNum;
     }
 };
