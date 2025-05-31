f25ce9b171875a22db541d7eb1f1a06e1a41cea1
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-05-31 23:41:28 +0530

    Time: 0 ms (100%), Space: 11.4 MB (36.86%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<int, vector<char>> int_to_char;
-        map<char, int> char_to_int;
-
-        for (char ch: s) char_to_int[ch]++;
-        for (auto it: char_to_int) int_to_char[it.second].push_back(it.first);
-
-        string res;
-
-        for (auto it: int_to_char){
-            for (auto el: it.second){
-                res += string(it.first, el);
-            }
+        map<char,int>mp;
+        for(char i:s){
+            mp[i]++;
         }
-
-        reverse(res.begin(), res.end());
-        return res;
+        priority_queue<pair<int, char>> pq;
+        for (auto& p : mp) {
+           pq.push({p.second, p.first});
+        }
+        string ans="";
+        while(!pq.empty()){
+            auto [x,y]=pq.top();
+            pq.pop();
+            ans+=string(x,y);
+        }
+        return ans;
     }
 };
