255e66c3f512f320ae6e5a6faa242951a411f148
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-05-31 23:43:27 +0530

    Time: 2 ms (64.09%), Space: 17.1 MB (6.57%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,14 +4,12 @@ public:
         unordered_map<char, int> freq;
         for (char ch : s) freq[ch]++;
 
-        // Max possible frequency is s.length()
         vector<vector<char>> buckets(s.size() + 1);
         for (auto& [ch, f] : freq) {
             buckets[f].push_back(ch);
         }
 
         string result;
-        // Go from high frequency to low
         for (int i = s.size(); i >= 1; --i) {
             for (char ch : buckets[i]) {
                 result += string(i, ch);
