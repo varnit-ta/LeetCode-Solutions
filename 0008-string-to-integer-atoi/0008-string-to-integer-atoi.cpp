f89356bea1473447846e720235059dfdcc7cbb51
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-05-31 23:52:32 +0530

    Time: 0 ms (100%), Space: 9.1 MB (56.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
-    bool isNum(char a){
-        if (a == '0' || a == '1' || a == '2' || a == '3' || a == '4' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9'){
-            return true;
-        }
-        return false;
-    }
-
     int myAtoi(string s) {
-        double result = 0;
-        bool isNeg = false;
-        bool hasSign = false;
-        int placeValue = 1;
-        bool hasBegin = false;
+        int ind = 0;
+        while (ind < s.size() && s[ind] == ' ') ind++;
 
-        for (int i = 0; i < s.length(); i++){
-            char element = s[i];
-
-            if (hasBegin && !isNum(element)){
-                break;
-            }else if (isNum(element)){
-                int num = element - '0';
-                result = result * 10;
-                result = result + num;
-                hasBegin = true;
-            }else if (!hasBegin && (element == '+' || element == '-') && !hasSign && isNum(s[i+1])){
-                if (element == '-'){
-                    isNeg = true;
-                    hasBegin = true;
-                }
-                hasSign = true;
-            }else if (result == 0 && element != ' '){
-                if (!isNum(element)){
-                    return 0;
-                }
-            }
+        bool flag = true;
+        if (ind < s.size() && s[ind] == '-') {
+            flag = false;
+            ind++;
+        } else if (ind < s.size() && s[ind] == '+') {
+            ind++;
         }
 
-        if (isNeg){
-            result = 0 - result;
+        while (ind < s.size() && s[ind] == '0') ind++;
+
+        int start = ind;
+        while (ind < s.size() && s[ind] >= '0' && s[ind] <= '9') ind++;
+
+        if (start == ind) return 0;
+
+        long long ans = 0;
+        for (int i = start; i < ind; i++) {
+            int digit = s[i] - '0';
+            ans = ans * 10 + digit;
+            
+            if (flag && ans > INT_MAX) return INT_MAX;
+            if (!flag && -ans < INT_MIN) return INT_MIN;
         }
 
-        if (result < pow(-2, 31)){
-            return pow(-2, 31);
-        }else if (result > (pow(2, 31) - 1)){
-            return pow(2, 31)-1;
-        }
+        if (!flag) ans = -ans;
 
-        return result;
+        return (int)ans;
     }
 };
