1b21373610ffb1214579065bfba448dd697efecc
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-03 22:22:24 +0530

    Time: 0 ms (100%), Space: 11.3 MB (27.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result = "";
-        string temp = "";
+        vector<string> words;
+        string temp;
 
         for (char el : s) {
             if (el != ' ') {
                 temp += el;
-            } else if (!temp.empty()) { 
-                if (!result.empty()) {
-                    result = temp + " " + result; 
-                } else {
-                    result = temp;
-                }
-                temp = ""; 
+            } else if (!temp.empty()) {
+                words.push_back(temp);
+                temp.clear();
             }
         }
-
+        
         if (!temp.empty()) {
-            if (!result.empty()) {
-                result = temp + " " + result;
-            } else {
-                result = temp;
-            }
+            words.push_back(temp);
+        }
+
+        reverse(words.begin(), words.end());
+
+        string result;
+        for (int i = 0; i < words.size(); ++i) {
+            result += words[i];
+            if (i < words.size() - 1) result += ' ';
         }
 
         return result;
