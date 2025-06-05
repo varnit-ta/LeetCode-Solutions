754da75f237408c671b4f7723013d9d2961ec2af
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-05 23:40:07 +0530

    Time: 3 ms (74.21%), Space: 18 MB (78.44%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        if (numCourses == 1) return {0};
+
+        vector<int> ans;
+        vector<int> inNodes(numCourses, 0);
+        vector<vector<int>> graph(numCourses, vector<int>());
+
+        for (int i = 0; i < prerequisites.size(); i++){
+            int a = prerequisites[i][0], b = prerequisites[i][1];
+            graph[b].push_back(a);
+            inNodes[a]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inNodes[i] == 0){ 
+                q.push(i);
+                ans.push_back(i);
+            }
+        }
+
+        while (!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for (int neigh: graph[node]){
+                inNodes[neigh]--;
+                if (inNodes[neigh] == 0){
+                    q.push(neigh);
+                    ans.push_back(neigh);
+                }
+            }
+        }
+
+        return (ans.size() == numCourses) ? ans : vector<int>();
+    }
+};
