7d49a2b59dc79b9943104fd99a038da22ef15926
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-05 23:24:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (28.6%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int vertices = graph.size();
+        vector<int> color(vertices, -1);
+
+        for (int i = 0; i < vertices; ++i) {
+            if (color[i] == -1) {
+                stack<pair<int, int>> stk;
+                stk.push({i, 0});
+                color[i] = 0;
+
+                while (!stk.empty()) {
+                    auto [node, clr] = stk.top();
+                    stk.pop();
+
+                    for (auto neigh : graph[node]) {
+                        if (color[neigh] == -1) {
+                            color[neigh] = (clr == 0 ? 1 : 0);
+                            stk.push({neigh, color[neigh]});
+                        } else if (clr == color[neigh]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
