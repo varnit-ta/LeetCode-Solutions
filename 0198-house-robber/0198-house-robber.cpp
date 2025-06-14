9b40f96f2e65f0fe612fb0576582d85f7634cfd7
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-14 22:00:11 +0530

    Time: 3 ms (6.07%), Space: 10.8 MB (5.92%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+
+        int n = nums.size();
+        vector<int> dp(nums.begin(), nums.end());
+
+
+        for (int i = 2; i < nums.size(); i++) 
+            dp[i] += max(dp[i - 2], dp[i - 1] - nums[i - 1]);
+        
+        return max(dp[n -1], dp[n - 2]);
+    }
+};
