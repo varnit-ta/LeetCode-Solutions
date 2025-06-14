33aa5d1a0b0ecd5debec7666823f27a4acb5d7e6
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-14 22:02:42 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,7 +6,6 @@ public:
         int n = nums.size();
         vector<int> dp(nums.begin(), nums.end());
 
-
         for (int i = 2; i < nums.size(); i++) 
             dp[i] += max(dp[i - 2], dp[i - 1] - nums[i - 1]);
         
