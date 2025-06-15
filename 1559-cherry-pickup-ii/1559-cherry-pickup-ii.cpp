a0e1e55b08e0ce4f8114c74c97fadebcc345fb42
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-15 13:10:59 +0530

    Time: 88 ms (8.6%), Space: 21 MB (52.54%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(cols, 0)));
+        vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(cols, -1)));
 
-        for (int r = rows - 1; r >= 0; --r) {
-            for (int c1 = 0; c1 < cols; ++c1) {
-                for (int c2 = 0; c2 < cols; ++c2) {
-                    int max_candy = 0;
-                    for (int d1 = -1; d1 <= 1; ++d1) {
-                        for (int d2 = -1; d2 <= 1; ++d2) {
-                            int nc1 = c1 + d1, nc2 = c2 + d2;
-                            if (nc1 >= 0 && nc1 < cols && nc2 >= 0 && nc2 < cols && r + 1 < rows) {
-                                max_candy = max(max_candy, dp[r + 1][nc1][nc2]);
-                            }
-                        }
+        function<int(int, int, int)> dfs = [&](int r, int c1, int c2) -> int {
+            if (c1 < 0 || c1 >= cols || c2 < 0 || c2 >= cols)
+                return INT_MIN;
+
+            if (dp[r][c1][c2] != -1)
+                return dp[r][c1][c2];
+
+            int result = 0;
+            result += (c1 == c2) ? grid[r][c1] : grid[r][c1] + grid[r][c2];
+
+            if (r != rows - 1) {
+                int max_candy = INT_MIN;
+                for (int d1 = -1; d1 <= 1; ++d1) {
+                    for (int d2 = -1; d2 <= 1; ++d2) {
+                        max_candy = max(max_candy, dfs(r + 1, c1 + d1, c2 + d2));
                     }
-                    int cherries = (c1 == c2) ? grid[r][c1] : grid[r][c1] + grid[r][c2];
-                    dp[r][c1][c2] = cherries + max_candy;
                 }
+                result += max_candy;
             }
-        }
-        return dp[0][0][cols - 1];
+
+            return dp[r][c1][c2] = result;
+        };
+
+        return dfs(0, 0, cols - 1);
     }
 };
