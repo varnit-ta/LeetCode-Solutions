c5e3ead74107e3ff0b11069ac5a73ac0205ec7a6
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-17 22:06:07 +0530

    Time: 46 ms (37.26%), Space: 48.8 MB (39.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
         const int INF = 1e9;
-        vector<int> dp(amount + 1, INF);
-        dp[0] = 0;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, INF));
 
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                if (dp[j - coin] != INF)
-                    dp[j] = min(dp[j], dp[j - coin] + 1);
+        // Base Case Initialization:
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0)
+                dp[0][t] = t / coins[0];
+        }
+
+        // Fill the DP table
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int notTake = dp[i - 1][t];
+                int take = INF;
+                if (coins[i] <= t)
+                    take = 1 + dp[i][t - coins[i]]; // use same coin again
+                dp[i][t] = min(take, notTake);
             }
         }
 
-        return (dp[amount] == INF) ? -1 : dp[amount];
+        return dp[n - 1][amount] == INF ? -1 : dp[n - 1][amount];
     }
 };
