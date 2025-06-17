797a0ffb0de92a6b3e5e96cd0be18c81b6c84948
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-17 19:30:45 +0530

    Time: 4 ms (87.69%), Space: 10.3 MB (82.38%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,16 @@
+int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned long long> dp(amount+1, 0);
+
+        dp[0] = 1;
+
+        for (auto& coin: coins)
+            for (int i = coin; i <= amount; i++)
+                dp[i] += dp[i - coin];
+
+        return dp[amount];
+    }
+};
