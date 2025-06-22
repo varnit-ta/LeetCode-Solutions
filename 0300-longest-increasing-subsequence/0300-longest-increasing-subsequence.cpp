81f45450a05935c62b8f5aa47c2c51d952b746b1
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-22 11:53:41 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,33 @@
+#ifdef LOCAL
+#else
+#define debug(...) 42
+#endif
+
+#define ll long long
+#define pb emplace_back
+#define F first
+#define S second
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<vector<int>>
+#define vll vector<vector<ll>>
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define FORN(i, a, b) for (int i = a; i >= b; --i)
+#define FORR(x, a) for (auto& x : a)
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
+        vector<int> arr;
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
+        for (int x: nums){
+            auto it = lower_bound(all(arr), x);
+
+            if (it == arr.end()) arr.pb(x);
+            else *it = x;
         }
 
-        return *max_element(dp.begin(), dp.end());
+        return arr.size();
     }
 };
