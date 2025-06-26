12f940ee6616f91d678f5a9d969c7a8eddd56687
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-06-27 02:08:52 +0530

    Time: 2934 ms (5.16%), Space: 89.4 MB (26.44%) - LeetHub

---
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dp(m, vector<int>(5000, -1));
+
+        function<int(int, int)> f = [&](int r, int currSum){
+            if (r == m) return abs(currSum - target);
+            if (dp[r][currSum] != -1) return dp[r][currSum];
+
+            int mini = INT_MAX;
+
+            for (int i = 0; i < n; i++){
+                int sum = f(r + 1, currSum + mat[r][i]);
+                mini = min(mini, sum);
+            }
+
+            return dp[r][currSum] = mini;
+        };
+
+        return f(0, 0);
+    }
+};
