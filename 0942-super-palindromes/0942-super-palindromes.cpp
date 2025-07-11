a032d2ee63fecfa61b05773a3c0f653253cca0db
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-07-11 11:28:11 +0530

    Time: 95 ms (73.91%), Space: 10.3 MB (65.22%) - LeetHub

---
+++ 0942-super-palindromes/0942-super-palindromes.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    long long reverse(long long v) {
+        long long temp = 0;
+
+        while (v != 0) {
+            temp = temp * 10 + v % 10;
+            v /= 10;
+        }
+
+        return temp;
+    }
+
+    bool isPalindrome(long long v) { 
+        return v == reverse(v); 
+    }
+
+    int superpalindromesInRange(string left, string right) {
+        long long L = stol(left), R = stol(right);
+
+        int MAGIC = 100000, total = 0;
+
+        for (int i = 1; i <= MAGIC; i++) {
+            string s = to_string(i);
+
+            for (int i = s.size() - 1; i >= 0; i--)
+                s += s[i];
+
+            long long v = stol(s);
+            v *= v;
+
+            if (v > R)
+                break;
+
+            if (v >= L && isPalindrome(v))
+                total++;
+        }
+
+        for (int i = 1; i <= MAGIC; i++) {
+            string s = to_string(i);
+
+            for (int i = s.size() - 2; i >= 0; i--)
+                s += s[i];
+
+            unsigned long long v = stol(s);
+            v *= v;
+
+            if (v > R)
+                break;
+
+            if (v >= L && isPalindrome(v))
+                total++;
+        }
+
+        return total;
+    }
+};
