8ae2d5ae0cae705b699dbf81c1f1377c0d10865e
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-08-05 21:50:29 +0530

    Time: 17 ms (89.5%), Space: 17.7 MB (75.6%) - LeetHub

--- 1517-restore-the-array/1517-restore-the-array.cpp
+++ 1517-restore-the-array/1517-restore-the-array.cpp
@@ -30,7 +30,23 @@ public:
 
     int numberOfArrays(string s, int k) {
         int n = s.size();
-        vector<long long> dp(n, -1);
-        return f(dp, s, k, 0);
+        vector<long long> dp(n+1, 0);
+        dp[n] = 1;
+
+        for (int i = n-1; i >= 0; i--) {
+            if (s[i] == '0')
+                continue;
+
+            long long num = 0;
+
+            for (int j = i; j < n; j++) {
+                num = num * 10 + (s[j] - '0');
+                if (num > k) break;
+
+                dp[i] = (dp[i] + dp[j+1]) % MOD;
+            }
+        }
+
+        return dp[0];
     }
 };
