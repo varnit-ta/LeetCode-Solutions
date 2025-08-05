7b73fd0cab61ba5c34d64f07fa0e12b0183b0cb9
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-08-05 21:52:20 +0530

    Time: 23 ms (81.1%), Space: 17.7 MB (76.57%) - LeetHub

--- 1517-restore-the-array/1517-restore-the-array.cpp
+++ 1517-restore-the-array/1517-restore-the-array.cpp
@@ -2,32 +2,6 @@ int MOD = 1000000007;
 
 class Solution {
 public:
-    int f(vector<long long>& dp, string& s, int k, int i){
-        if (i == s.size())
-            return 1;
-
-        if (s[i] == '0')
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        long long num = 0;
-        dp[i] = 0;
-
-        for (int j = i; j < s.size(); j++) {
-            num = num * 10 + (s[j] - '0');
-
-            if (num <= k) {
-                dp[i] = (dp[i] + f(dp, s, k, j+1)) % MOD;
-            }else{
-                break;
-            }
-        }
-
-        return dp[i];
-    }
-
     int numberOfArrays(string s, int k) {
         int n = s.size();
         vector<long long> dp(n+1, 0);
