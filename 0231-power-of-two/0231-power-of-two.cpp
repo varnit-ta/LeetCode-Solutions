392b970352dac29931a079bc4db9433f77a0cecf
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-08-09 14:23:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;
+        if (n == 0)
+            return false;
+
+        bool found = false;
+
+        while (n != 0){
+            if (found)
+                return false;
+
+            if (n & 1)
+                found = true;
+
+            n = n >> 1;
+        }  
+
+        return true;
     }
 };
