00cbe0f27f0868ab801bd621b6ce7ab8aad8f1f5
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-08-14 12:40:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (77.37%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -5,6 +5,8 @@ public:
         string res;
 
         for (int i = 0; i < n-2; i++) {
+            if (!res.empty() && res[0] == s[i]) continue;
+
             if (s[i] == s[i+1] && s[i] == s[i+2]) {
                 string triplet = s.substr(i, 3);
 
