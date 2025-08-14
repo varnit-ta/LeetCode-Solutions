d09f73afe07d6ffec171a1f1e40825f295635949
Varnit Singh <116811148+varnit-ta@users.noreply.github.com>
2025-08-14 23:09:34 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.7%) - LeetHub

---
+++ 1825-find-minimum-time-to-finish-all-jobs/1825-find-minimum-time-to-finish-all-jobs.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool check(vector<int>& jobs, vector<int>& work, int k, int mid, int indx) {
+        if (indx == jobs.size())
+            return true;
+
+        for (int i = 0; i < k; i++) {
+            if (work[i] + jobs[indx] <= mid) {
+                work[i] += jobs[indx];
+                
+                if (check(jobs, work, k, mid, indx + 1)) 
+                    return true;
+
+                work[i] -= jobs[indx];
+            }
+            if (work[i] == 0) break;
+        }
+        return false;
+    }
+
+    int minimumTimeRequired(vector<int>& jobs, int k) {
+        sort(jobs.rbegin(), jobs.rend());
+        int low = *max_element(jobs.begin(), jobs.end());
+        int high = accumulate(jobs.begin(), jobs.end(), 0);
+        int ans = high;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            vector<int> work(k, 0); // fresh for each mid
+
+            if (check(jobs, work, k, mid, 0)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
